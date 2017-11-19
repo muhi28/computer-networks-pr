919013a492e6564cda473b2bdc1f7525f60e436f
thompson <thauer@edu.aau.at>
2017-11-19 15:53:41 +0100

    > HTTPServer fin >> Works, now cleaning & Pimping

--- UE5/5.1/HttpServer.java
+++ UE5/5.1/HttpServer.java
@@ -4,13 +4,20 @@ import java.net.Socket;
 
 public class HttpServer{
     private int port;
-    private String request, response;
+    private String request;
+    private File data;
     private ServerSocket ss;
     private Socket cs;
-    private BufferedReader in;
-    private FileOutputStream fos;
+    private BufferedReader inFromClient;
+    private BufferedInputStream bufferToClient;
+    private OutputStream os;
+    private byte[] byteBuffer;
+
+    private final String CLRF = "\r\n\r\n";
+    private final String httpResponseHeader = "HTTP/0.9 200 OK" + CLRF;
 
     private boolean debug_printWholeRequest = false;
+    private boolean debug_fetchFile = true;
 
     public HttpServer(int port){
         this.port = port;
@@ -28,10 +35,10 @@ public class HttpServer{
                 cs = ss.accept();
 
                 // Setup readable for GET/POST request
-                in = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+                inFromClient = new BufferedReader(new InputStreamReader(cs.getInputStream()));
 
                 // Expect input
-                request = in.readLine();
+                request = inFromClient.readLine();
                 System.out.println("Incomming Request:\t" + request);
 
                 // For Debugging: Print Request
@@ -39,27 +46,22 @@ public class HttpServer{
 
                 // Check for correct GET-Request
                 /** Example: http://localhost:6789/documentRoot/index.html**/
-                // TODO: Send the index.html aswell as its pictures
                 if(request.contains("GET")){
-//                    String answer = "Mai boi";
-  //                  response = "HTTP/1.1 200 OK\r\n\r\n" + answer;
-    //                cs.getOutputStream().write(response.getBytes("UTF-8"));
-
                     // Fetch the data
                     String path = request.substring(request.indexOf('/'), request.indexOf('H')-1);
-                    File data = fetchFile(path);
 
-                    // Transfer to client
-                byte[] mybyteArray = new byte[(int) data.length()];
-                FileInputStream fis = new FileInputStream(data);
-                BufferedInputStream bis = new BufferedInputStream(fis);
-                bis.read(mybyteArray,0,mybyteArray.length);
-                OutputStream os = cs.getOutputStream();
-                System.out.println("Sending: " + data);
-                os.write("HTTP/1.1 200 OK\r\n\r\n".getBytes("UTF-8"));
-                os.write(mybyteArray, 0, mybyteArray.length);
-                os.flush();
-                System.out.println("C4yourself");
+                    // TODO:
+                    /*if(){
+                        // Path matches a directory
+                    }else{
+                        // Path matches a simple File
+                        data = fetchFile(path);
+                    }*/
+
+                    //Setup Datatransfer
+                    setupTransfer();
+                    // Send Data
+                    sendData();
                 }
                 // After Transfer, terminate data
                 cs.close();
@@ -70,27 +72,44 @@ public class HttpServer{
         }
     }
 
+    private void sendData() throws IOException {
+        // Send Data to client
+        os = cs.getOutputStream();
+        os.write(httpResponseHeader.getBytes("UTF-8"));
+        os.write(byteBuffer, 0, byteBuffer.length);
+        os.flush();
+    }
+
+    private void setupTransfer() throws IOException{
+        // Transfer to client -- Buffer the datafile to send
+        byteBuffer = new byte[(int) data.length()];
+        bufferToClient = new BufferedInputStream(new FileInputStream(data));
+        bufferToClient.read(byteBuffer,0,byteBuffer.length);
+    }
+
     /**
      * @param path of the file requested
      * @return file of requested path
      */
     private File fetchFile(String path) {
+        // Create the path to our file(s)
         String pwd = System.getProperty("user.dir"); // Path up to working dir
         String psd = "/UE5/5.1";                     // Path up to source dir -- FIXME: needs a better solution
-        String pathToFile = pwd + psd + path;             // Concat Paths for full path to file
+        String pathToFile = pwd + psd + path;        // Concat Paths for full path to file
 
-
-        if(pathToFile.equals("/home/thompson/Uni/WS2017/RN-UE/Computer-Networks/UE5/5.1/documentRoot")){
-            System.out.println("Matching paths");
+        if(debug_fetchFile){
+            System.out.println("Working Dir:\n\t" + pwd);
+            System.out.println("Source Dir:\n\t" + psd);
+            System.out.println("WD + SD:\n\t" + pathToFile);
+            System.out.println("Absolute path: \n\t/home/thompson/Uni/WS2017/RN-UE/Computer-Networks/UE5/5.1/documentRoot");
         }
 
+        // Now link the file for real.
         File requestedFile = new File(pathToFile);
-        System.out.println("Pathing:\t\t\t"+  requestedFile.getPath());
-        System.out.println("Absolute path: \t\t/home/thompson/Uni/WS2017/RN-UE/Computer-Networks/UE5/5.1/documentRoot");
+        // Check existance
         if(!requestedFile.exists()){
             System.out.println("Non-Existant File.");
         }
-
         return requestedFile;
     }
 
@@ -106,7 +125,7 @@ public class HttpServer{
         System.out.println("--------- Full request ----------");
         while(!request.isEmpty()){
             System.out.println("\t"+request);
-            request = in.readLine();
+            request = inFromClient.readLine();
         }
         System.out.println("--------- End of printGetRequest ----------");
     }

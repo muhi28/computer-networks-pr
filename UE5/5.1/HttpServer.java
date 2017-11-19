3e2f4e285bed2bc32258627122b2e6eea7251d48
thompson <thauer@edu.aau.at>
2017-11-19 12:24:59 +0100

    > HTTP-Server init >> Needs refinement. Datatransfer works

---
+++ UE5/5.1/HttpServer.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServer{
+    private int port;
+    private String request, response;
+    private String get, path, http;     // As substrings of request
+    private ServerSocket ss;
+    private Socket cs;
+    private BufferedReader in;
+    private OutputStream out;
+
+    private boolean debug_printWholeRequest = false;
+
+    public HttpServer(int port){
+        this.port = port;
+
+    }
+
+    public void start(){
+        try{
+            // Setup SS
+            ss = new ServerSocket(this.port);
+
+            // Await requests
+            while(true){
+                // Setup CS
+                cs = ss.accept();
+
+                // Setup readable for GET/POST request
+                in = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+
+                // Expect input
+                request = in.readLine();
+                System.out.println("Incomming Request:\t" + request);
+
+                // For Debugging: Print Request
+                if(debug_printWholeRequest){ printGetRequest(); }
+
+                // Check for correct GET-Request
+                /** Example: http://localhost:6789/documentRoot/index.html**/
+                if(request.contains("GET")){
+                    String answer = "Mai boi";
+                    response = "HTTP/1.1 200 OK\r\n\r\n" + answer;
+                    cs.getOutputStream().write(response.getBytes("UTF-8"));
+                }
+
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+
+        }
+    }
+
+    /**
+     * Method entirely for debugging on fetched request
+     * @throws Exception
+     */
+    private void printGetRequest() throws Exception{
+        if(request.contains("GET")){    System.out.println("- Request contains GET");  }
+        if(request.contains("/")){      System.out.println("- Request contains some Path"); }
+        if(request.contains("HTTP")){   System.out.println("- Request contains HTTP-Format"); }
+        System.out.println("--------- Full request ----------");
+        while(!request.isEmpty()){
+            System.out.println("\t"+request);
+            request = in.readLine();
+        }
+        System.out.println("--------- End of printGetRequest ----------");
+    }
+
+
+    public static void main(String[]args){
+        int port = 6789;
+        HttpServer hs = new HttpServer(port);
+        hs.start();
+    }
+}
